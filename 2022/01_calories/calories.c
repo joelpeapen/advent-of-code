4614e94b36eb0242d6921d6b06672585edb11beb
Joel Eapen <67512946+joelpeapen@users.noreply.github.com>
2023-06-18 16:37:52 +0530

    Initial commit

---
+++ 2022/01_calories/calories.c
@@ -0,0 +1,39 @@
+// elf calories
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int main() {
+    FILE* file = NULL;
+    char elfCurrent[SIZE];
+    int maxCal = 0, theElf = 0, elfCount = 1;
+
+    if (!(file = fopen("input.txt", "r"))) {
+        puts("FILE DID NOT OPEN");
+        exit(EXIT_FAILURE);
+    }
+
+    while (!feof(file)) {
+        int totalElf = 0;
+        fgets(elfCurrent, SIZE, file);
+        printf("\n Elf %i\n -------\n", elfCount);
+        // detect EOF again for last calorie
+        for (int i = 1; elfCurrent[0] != '\n' && !feof(file); i++) {
+            printf("%2i: %s", i, elfCurrent);
+            totalElf += atoi(elfCurrent);
+            fgets(elfCurrent, SIZE, file);
+        }
+        printf("TOTAL: %i", totalElf);
+        if (totalElf > maxCal) {
+            maxCal = totalElf;
+            theElf = elfCount;
+        }
+        elfCount++;
+        puts("");
+    }
+
+    fclose(file);
+
+    printf("\nelf %i has %i calories", theElf, maxCal);
+}
---
+++ 2022/01_calories/calories2.c
@@ -0,0 +1,49 @@
+// elf calories
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int main() {
+    FILE* file = NULL;
+    int calories[3] = { 0 }, elves[3] = { 0 }, elfCount = 1;
+    char elfCurrent[SIZE]; // char for fgets
+
+    if (!(file = fopen("01_input.txt", "r"))) {
+        puts("FILE DID NOT OPEN");
+        exit(EXIT_FAILURE);
+    }
+
+    while (!feof(file)) {
+        int totalElf = 0;
+        fgets(elfCurrent, SIZE, file);
+        printf("\n Elf %i\n -------\n", elfCount);
+        // detect EOF again for last elf calorie
+        for (int i = 1; elfCurrent[0] != '\n' && !feof(file); i++) {
+            printf("%2i: %s", i, elfCurrent);
+            totalElf += atoi(elfCurrent);
+            fgets(elfCurrent, SIZE, file);
+        }
+        printf("TOTAL: %i", totalElf);
+        if (totalElf > calories[0]) {
+            calories[2] = calories[1];
+            calories[1] = calories[0];
+            calories[0] = totalElf;
+            elves[2] = elves[1];
+            elves[1] = elves[0];
+            elves[0] = elfCount; // biggest elf
+        }
+        elfCount++;
+        puts("");
+    }
+
+    fclose(file);
+
+    int totalElf = 0;
+    for (size_t i = 0; i < 3; i++) {
+        printf("\nElf %3i: %i", elves[i], calories[i]);
+        totalElf += calories[i];
+    }
+    printf("\n\nElf %i has the most calories", elves[0]);
+    printf("\nSum of 3 largest elves: %i", totalElf);
+}
