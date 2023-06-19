4e8e45c597d0526d56fe2e8936e71636273004d9
Joel Eapen <joelpeapen@protonmail.com>
2023-06-19 18:47:19 +0530

    (day): 2022/day2

--- 2022/02_rock-paper-scissors/rock-paper-scissors.c
+++ 2022/02_rock-paper-scissors/rock-paper-scissors.c
@@ -3,13 +3,13 @@
 
 enum Hands { ROCK = 1, PAPER, SCISSORS };
 
-void gethand(char *a, char *me);
-char win(const char a, const char me);
+void gethand(char *a, char *b);
+char win(const char a, const char b);
 
 int main() {
     FILE* file = NULL;
     int score = 0, total = 0, pos = -4;
-    char a = 0, me = 0;
+    char a = 0, b = 0;
 
     if (!(file = fopen("02_input.txt", "r"))) {
         puts("FILE DID NOT OPEN");
@@ -18,42 +18,25 @@ int main() {
 
     while (!feof(file)) {
         fseek(file, pos += 4, SEEK_SET);
-        fscanf(file, "%c %c", &a, &me);
-        printf("\n%c %c\t", a, me);
-        gethand(&a, &me);
-        total += win(a, me) + me;
-        printf("+ %i\tscore: %i", me, total);
+        fscanf(file, "%c %c", &a, &b);
+        printf("\n%c %c\t", a, b);
+        gethand(&a, &b);
+        total += win(a, b) + b;
+        printf("+ %i\tscore: %i", b, total);
     }
 
+    // 6 ∵ fseek at start of while and runs extra time
     printf("\n\n\tTotal: %i", total - 6);
 }
 
-void gethand(char *a, char *me) {
-    if (*a == 'A') {
-        *a = ROCK;
-    } else if (*a == 'B') {
-        *a = PAPER;
-    } else if (*a == 'C') {
-        *a = SCISSORS;
-    }
-
-    if (*me == 'X') {
-        *me = ROCK;
-    } else if (*me == 'Y') {
-        *me = PAPER;
-    } else if (*me == 'Z') {
-        *me = SCISSORS;
-    }
-}
-
-char win(const char a, const char me) {
-    if (a == me && a != '\0' && me != '\0') {
+char win(const char a, const char b) {
+    if (a == b && a != '\0' && b != '\0') {
         printf("DRAW +3\t");
         return 3;
     }
 
     if (a == ROCK) {
-        if (me == PAPER) {
+        if (b == PAPER) {
             printf("WIN  +6\t");
             return 6;
         } else {
@@ -62,7 +45,7 @@ char win(const char a, const char me) {
         }
     }
 
-    if (me == ROCK) {
+    if (b == ROCK) {
         if (a == PAPER) {
             printf("LOST +0\t");
             return 0;
@@ -72,15 +55,33 @@ char win(const char a, const char me) {
         }
     }
 
-    if (a == SCISSORS && me == PAPER) {
+    if (a == SCISSORS && b == PAPER) {
         printf("LOST +0\t");
         return 0;
     }
 
-    if (me == SCISSORS && a == PAPER) {
+    if (b == SCISSORS && a == PAPER) {
         printf("WIN  +6\t");
         return 6;
     }
 
     return 0;
+}
+
+void gethand(char *a, char *b) {
+    if (*a == 'A') {
+        *a = ROCK;
+    } else if (*a == 'B') {
+        *a = PAPER;
+    } else if (*a == 'C') {
+        *a = SCISSORS;
+    }
+
+    if (*b == 'X') {
+        *b = ROCK;
+    } else if (*b == 'Y') {
+        *b = PAPER;
+    } else if (*b == 'Z') {
+        *b = SCISSORS;
+    }
 }
---
+++ 2022/02_rock-paper-scissors/rock-paper-scissors2.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum Hands { ROCK = 1, PAPER, SCISSORS };
+
+void gethand(char *a, char *me);
+char win(const char a, const char me);
+
+int main() {
+    FILE* file = NULL;
+    int score = 0, total = 0, pos = -4;
+    char a = 0, me = 0;
+
+    if (!(file = fopen("input.txt", "r"))) {
+        puts("FILE DID NOT OPEN");
+        exit(EXIT_FAILURE);
+    }
+
+    while (!feof(file)) {
+        fseek(file, pos += 4, SEEK_SET);
+        fscanf(file, "%c %c", &a, &me);
+        printf("\n%c %c\t", a, me);
+        gethand(&a, &me);
+        total += win(a, me) + me;
+        printf("+ %i\tscore: %i", me, total);
+    }
+
+    // 7 ∵ fseek at start of while and runs extra time
+    printf("\n\n\tTotal: %i", total - 7);
+}
+
+char win(const char a, const char me) {
+    if (a == me && a != '\0' && me != '\0') {
+        printf("DRAW +3\t");
+        return 3;
+    }
+
+    if (a == ROCK) {
+        if (me == PAPER) {
+            printf("WIN  +6\t");
+            return 6;
+        } else {
+            printf("LOST +0\t");
+            return 0;
+        }
+    }
+
+    if (me == ROCK) {
+        if (a == PAPER) {
+            printf("LOST +0\t");
+            return 0;
+        } else {
+            printf("WIN  +6\t");
+            return 6;
+        }
+    }
+
+    if (a == SCISSORS && me == PAPER) {
+        printf("LOST +0\t");
+        return 0;
+    }
+
+    if (me == SCISSORS && a == PAPER) {
+        printf("WIN  +6\t");
+        return 6;
+    }
+
+    return 0;
+}
+
+void gethand(char *a, char *b) {
+    if (*a == 'A') {
+        *a = ROCK;
+    } else if (*a == 'B') {
+        *a = PAPER;
+    } else if (*a == 'C') {
+        *a = SCISSORS;
+    }
+
+    if (*a == ROCK) {
+        if (*b == 'X') {
+            *b = SCISSORS;
+        } else if (*b == 'Y') {
+            *b = ROCK;
+        } else {
+            *b = PAPER;
+        }
+    }
+
+    if (*a == PAPER) {
+        if (*b == 'X') {
+            *b = ROCK;
+        } else if (*b == 'Y') {
+            *b = PAPER;
+        } else {
+            *b = SCISSORS;
+        }
+    }
+
+    if (*a == SCISSORS) {
+        if (*b == 'X') {
+            *b = PAPER;
+        } else if (*b == 'Y') {
+            *b = SCISSORS;
+        } else {
+            *b = ROCK;
+        }
+    }
+}
